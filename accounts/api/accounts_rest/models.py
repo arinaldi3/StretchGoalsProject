d400716391af43f7e4d26757788460ee84b5969c
Vindasu <alexleeadams@aol.com>
2022-08-19 16:32:42 -0400

    attempt to fix docker for group

--- accounts/api/accounts_rest/models.py
+++ accounts/api/accounts_rest/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+from django.db import models
+from django.contrib.auth.models import AbstractUser, PermissionsMixin
+from django.core.validators import int_list_validator
 # Create your models here.
+
+
+class Student(AbstractUser):
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return f"{self.email}"
+
+
+class Instructor(AbstractUser):
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return f"{self.email}"
