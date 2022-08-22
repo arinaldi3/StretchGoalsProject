90030b28d8c1d4642b04061339622f80ce6664b8
Josh <joshlee0201@gmail.com>
2022-08-22 14:56:02 -0700

    updated urls in accounts

--- accounts/api/accounts_rest/urls.py
+++ accounts/api/accounts_rest/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from accounts.api.accounts_rest.views import api_instructor, api_student, api_current_user
+from .views import api_instructor, api_student, api_current_user, api_instructors, api_students
 
 
 urlpatterns = [
     path('instructors/', api_instructors, name='api_instructors'),
-    path('instructors/', api_instructor, name='api_instructor'),
+    path('instructors/<int:pk>/', api_instructor, name='api_instructor'),
     path('students/', api_students, name='api_students'),
     path('students/<int:pk>/', api_student, name='api_student'),
     path('account/<str:username>', api_current_user, name="api_current_user")
