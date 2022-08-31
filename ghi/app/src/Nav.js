6f9f30f8fc16ce096faa99bd152eeee5ed10c7d6
Ava <avarinaldi5@gmail.com>
2022-08-31 14:00:19 -0400

    Working on login/logout functionalities

--- ghi/app/src/Nav.js
+++ ghi/app/src/Nav.js
@@ -1,8 +1,12 @@
 import { NavLink } from 'react-router-dom';
 import { AuthProvider, useToken } from './Authentication';
+import React, { useState } from 'react';
 
 function Nav() {
   const [token, login, logout] = useToken();
+  // const [students, setStudents] = useState();
+  const [instructors, setInstructors] = useState();
+
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid">
@@ -12,15 +16,20 @@ function Nav() {
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            { token &&
           <li className="nav-item">
               <NavLink className="nav-link" aria-current="page" to="list/instructors">Meet the Instructors</NavLink>
           </li>
+          }
+          { token &&
           <li className="nav-item">
               <NavLink className="nav-link" aria-current="page" to="list/classes">List of Classes</NavLink>
-          </li>
+          </li>}
+          {token &&
           <li className="nav-item">
               <NavLink className="nav-link" aria-current="page" to="new/class">Create a Class</NavLink>
-          </li>
+          </li>}
+          { !token &&
           <li className="nav-item dropdown">
             <a className="nav-link dropdown-toggle" href='#' role="button" data-bs-toggle="dropdown" aria-expanded="false" aria-haspopup="true">Sign up</a>
             <ul className="dropdown-menu">
@@ -28,12 +37,24 @@ function Nav() {
               <li><NavLink className="dropdown-item" to="new/instructor">Sign up as an instructor</NavLink></li>
             </ul>
           </li>
+          }
+          { !token &&
           <li className="nav-item">
               <NavLink className="nav-link" aria-current="page" to="/login">Login</NavLink>
-          </li>
+          </li>}
+          { token &&
           <li>
             <button onClick={logout} className="btn btn-primary">Logout</button>
-          </li>
+          </li>}
+          {token &&
+          <li className="nav-item">
+              <NavLink className="nav-link" aria-current="page" to="portal/student">My Profile</NavLink>
+          </li>}
+          
+          {token &&
+          <li className="nav-item">
+              <NavLink className="nav-link" aria-current="page" to="portal/instructor">Instructor Profile</NavLink>
+          </li>}
           </ul>
         </div>
       </div>
